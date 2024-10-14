5082e8dca9eed9028f0b5d047b0ad3683f4eb699
CarlosDBCloudWeb <carlos123wuli@gmail.com>
2024-10-13 23:26:33 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-// script.js
 function validateLogin() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    
-    if (username === 'carlos' && password === '123456') {
-      alert('登录成功！');
-      // 在这里可以添加重定向到首页的代码，例如：window.location.href = '/home';
-    } else {
-      alert('El nombre de usuario o la contraseña son incorrectos, ¡inténtalo de nuevo!        (div) Wrong username or password, please try again!');
-    }
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  // 这里可以添加你的登录验证逻辑
+  if (username === 'admin' && password === '123456') {
+    // 验证成功后跳转到第二个页面
+    window.location.href = '/OpenUi/home.html'; // 将此替换为你想要跳转的页面地址
+    return false; // 阻止表单提交
+  } else {
+    alert('Usuario o contraseña incorrectos');
+    return false; // 阻止表单提交
   }
-  
+}
