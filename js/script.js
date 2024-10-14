9ffc2f45c30e3fddf9b6f21db4619833380d0494
CarlosDBCloudWeb <carlos123wuli@gmail.com>
2024-10-13 23:28:10 -0600

    Update Account

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function validateLogin() {
   const password = document.getElementById('password').value;
 
   // 这里可以添加你的登录验证逻辑
-  if (username === 'admin' && password === '123456') {
+  if (username === 'carlos' && password === '123456') {
     // 验证成功后跳转到第二个页面
     window.location.href = '/OpenUi/home.html'; // 将此替换为你想要跳转的页面地址
     return false; // 阻止表单提交
